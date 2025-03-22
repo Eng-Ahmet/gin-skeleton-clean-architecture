222dfdad266a20b816e44b5a160b2a3bfa083df9
ahmet <a.h.ahmet.es@gmail.com>
2025-03-23 00:05:55 +0100

    done

--- test/main_test.go
+++ test/main_test.go
@@ -1,7 +1,12 @@
-package main
+package test
 
 import "testing"
 
 func TestMain(t *testing.T) {
-	t.Log("Test ran successfully")
+	t.Run("1.test", TestGetInfoLogger)
+	println("Test Get Info Logger done")
+	t.Run("2.test", TestGetWarningLogger)
+	println("Test Get Warning Logger done")
+	t.Run("3.test", TestGetErrorLogger)
+	println("Test Get Error Logger done")
 }
